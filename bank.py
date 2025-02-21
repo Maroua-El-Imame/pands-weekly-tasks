98830bb0de62a8dbb3d21c7e687b1bdc75654b4f
Maroua El Imame <g00472980@atu.ie>
2025-02-21 18:33:35 +0000

    Weekly Tasks 02

---
+++ bank.py
@@ -0,0 +1,10 @@
+# bank.py
+# this program prints in euros the sum of two amounts in cents
+# by Maroua
+
+# a & b amounts displayed in cents , sum is displayed in euros
+
+amount1 = 65
+amount2 = 180
+
+print(f"The sum is € {(int(amount1)+int(amount2))/100}")
