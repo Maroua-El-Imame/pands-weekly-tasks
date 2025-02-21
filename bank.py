e9e08bbdfc0799402dac561187cf273e64ab9d38
Maroua El Imame <g00472980@atu.ie>
2025-02-21 20:48:14 +0000

    Weekly Tasks 02

--- bank.py
+++ bank.py
@@ -2,17 +2,28 @@
 # this program prints in euros the sum of two amounts in cents
 # by Maroua
 
-# amount1 & amount2 amounts displayed in cents , sum is displayed in euros
-
 amount1 = 65
 amount2 = 180
+amountinCents=amount1+amount2
 # or
-amountinEuros = (amount1+amount2)/100
+amountinEuros = (amountinCents)/100
 
+'''
+#Method 1
+print(f" ¢  {(amountinCents)}  = € {(amount1+amount2)/100}")
 
-# Method 1
-print(f"The sum is € {(amount1+amount2)/100}")
+print(f" ¢  {(amount1)+(amount2)}  = € {(amount1+amount2)/100}")
 
+# Method 2
 
-#Method 2
-print(f"The sum is € {amountinEuros}")
+print(f" The sum is € {(amount1+amount2)/100}")
+print(f" The sum is € {amountinEuros}")
+
+print(f" ¢ {int(amount1)+int(amount2)} is equal to € {(amount1+amount2)/100}")
+
+# Method 3
+
+print(f" ¢ {(amount1+amount2)} is € {(amountinEuros)}")
+
+'''
+print(f" The sum of ¢ {(amount1)} +  ¢ {(amount2)} is € {(amount1+amount2)/100}")
