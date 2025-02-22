cd484482701c8a6b30c9cc1d7ab674d1afba8019
Maroua El Imame <g00472980@atu.ie>
2025-02-22 16:02:09 +0000

    description added & code refined

--- accounts.py
+++ accounts.py
@@ -1,3 +1,8 @@
+# acounts.py
+# this programs reads in a 10 character account number and outputs the account number with only the last 4 digits showing.
+# Author : Maroua El imame
+
+
 accountnumber= "1234567890"
 
 
--- bank.py
+++ bank.py
@@ -8,22 +8,4 @@ amountinCents=amount1+amount2
 # or
 amountinEuros = (amountinCents)/100
 
-'''
-#Method 1
-print(f" ¢  {(amountinCents)}  = € {(amount1+amount2)/100}")
-
-print(f" ¢  {(amount1)+(amount2)}  = € {(amount1+amount2)/100}")
-
-# Method 2
-
-print(f" The sum is € {(amount1+amount2)/100}")
-print(f" The sum is € {amountinEuros}")
-
-print(f" ¢ {int(amount1)+int(amount2)} is equal to € {(amount1+amount2)/100}")
-
-# Method 3
-
-print(f" ¢ {(amount1+amount2)} is € {(amountinEuros)}")
-
-'''
 print(f" The sum of ¢ {(amount1)} +  ¢ {(amount2)} is € {(amount1+amount2)/100}")
