ddc9547fb1ad49cd9402fabbd9aed749cbf264e8
Maroua El Imame <g00472980@atu.ie>
2025-03-03 15:53:12 +0000

    Weekly task 05

---
+++ weekday.py
@@ -0,0 +1,5 @@
+# This program outputs whether or not today is a weekday.
+# If this program is run on a Thursday, it should print ' Yes, unfortunately today is a weekday '
+# If this program is run on a Saturday, it should print ' It is the weekend, yay! '
+
+
