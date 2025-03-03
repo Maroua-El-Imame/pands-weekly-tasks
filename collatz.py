14605203dac148f8c327d1012915556eaeeb861d
Maroua El Imame <g00472980@atu.ie>
2025-03-03 15:45:59 +0000

    Weekly task 04

--- collatz.py
+++ collatz.py
@@ -3,19 +3,3 @@
 # The principle of calculation : The next value is calculated by taking the current value, if 
 #  value is even [divide it by 2], if it is odd [multiply it by 3 and add on]. The program end if the current value is one.
 
-'''
-val = input(" Insert a positive Integer ")
-
-'''
-'''
-count = 0 
-while(count< 10):
-    print   ("count is ", count )
-    count = count + 1
-
-    '''
-a = 1
-b = 2
-remainder = a % b
-if  remainder >0:
-    print(a)
