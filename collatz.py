e34bcc4e725007e3e0220c5d88edc3d98e4d338e
Maroua El Imame <g00472980@atu.ie>
2025-03-03 15:45:02 +0000

    Weekly task 04

---
+++ collatz.py
@@ -0,0 +1,21 @@
+# Collatz.py
+# This program asks the user to insert any positive integer and outputs the values 10 5 16 8 4 2 1
+# The principle of calculation : The next value is calculated by taking the current value, if 
+#  value is even [divide it by 2], if it is odd [multiply it by 3 and add on]. The program end if the current value is one.
+
+'''
+val = input(" Insert a positive Integer ")
+
+'''
+'''
+count = 0 
+while(count< 10):
+    print   ("count is ", count )
+    count = count + 1
+
+    '''
+a = 1
+b = 2
+remainder = a % b
+if  remainder >0:
+    print(a)
