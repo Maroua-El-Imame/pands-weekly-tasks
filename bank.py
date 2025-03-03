4c78a8d925b753e73bf1d7274c4845a613e3f3b0
Maroua El Imame <g00472980@atu.ie>
2025-03-03 14:06:25 +0000

    Weekly task 02

--- bank.py
+++ bank.py
@@ -5,7 +5,6 @@
 amount1 = 65
 amount2 = 180
 amountinCents=amount1+amount2
-# or
 amountinEuros = (amountinCents)/100
 
-print(f" The sum of ¢ {(amount1)} +  ¢ {(amount2)} is € {(amount1+amount2)/100}")
+print(f" The sum of ¢{(amount1)} and ¢{(amount2)} is € {amountinEuros}")
