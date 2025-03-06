9640a70878a1d3a77fce7aa6f6d1cb6daf0948b9
Maroua El Imame <g00472980@atu.ie>
2025-03-06 17:54:45 +0000

    Weekly task 04

--- collatz.py
+++ collatz.py
@@ -3,3 +3,14 @@
 # The principle of calculation : The next value is calculated by taking the current value, if 
 #  value is even [divide it by 2], if it is odd [multiply it by 3 and add on]. The program end if the current value is one.
 
+
+
+number = int(input("enter an integer:"))
+
+if (number % 2) == 0:
+    print (number//2)
+else:
+    print((3*number)+1)
+
+# Need more details how to insert append so the numbers are kept for the end, and then displayed as a serie.
+
