b5b0c3a3dc70fff5a560ce20ecd39acb2b7e0852
Maroua El Imame <g00472980@atu.ie>
2025-03-10 01:26:28 +0000

    Weekly task 04

--- collatz.py
+++ collatz.py
@@ -1,16 +1,40 @@
 # Collatz.py
-# This program asks the user to insert any positive integer and outputs the values 10 5 16 8 4 2 1
-# The principle of calculation : The next value is calculated by taking the current value, if 
-#  value is even [divide it by 2], if it is odd [multiply it by 3 and add on]. The program end if the current value is one.
+# This program asks the user to enter a positive integer.
+# The next value is calculated by taking the number entered, then depending on the value :
+# if value is even [the program divide the named value by 2
+# if it is odd [ the program multiplies the odd number by 3 and adds 1 ]
+# The program ends when the current value is one.
 
 
 
-number = int(input("enter an integer:"))
+                                                # Variables and Values explained :          
 
-if (number % 2) == 0:
-    print (number//2)
+
+def collatz(number):                            # def : defining the function                                                         
+
+    if number % 2 == 0:                         # even number : defined by the value entered, when devided the remainder is equal to 0 
+        evenNumberResult=number//2              # when even number is entered, it calculates the value of the number divided by 2
+        numberList.append(evenNumberResult)     # the append() method will add the number result to the end of a list
+        return(evenNumberResult)                # return statement it tells the function what value should be returned
+    elif number % 2 != 0:                       # not even number = odd number
+        oddNumberResult = 3 * number + 1        # when odd number is entered, it calculates the value of the number multiplied by 3 then adding 1 
+        numberList.append(oddNumberResult)      # the append() method will add the number result to the end of a list
+        return(oddNumberResult)                 # return statement it tells the function what value should be returned
+
+# Program starts here.        
+
+number = int(input('Please enter a positive integer : '))    # Variable
+numberList = [number]                                        # Numbers list
+        
+while number != 1:                              # The program runs in loop as long as the statement is True. The program, then ends when number 1 is finally reached ( statement is False)
+            number = collatz(int(number))
 else:
-    print((3*number)+1)
+         print(numberList)
+        
+# The program outputs the following :
+# Please enter a positive integer : 10
+# 10 5 16 8 4 2 1
 
-# Need more details how to insert append so the numbers are kept for the end, and then displayed as a serie.
-
+# Resources:
+# url : https://stackoverflow.com/questions/33508034/making-a-collatz-program-automate-the-boring-stuff#:~:text=37-,def%20collatz(number)%3A,-if%20number%20%25
+# elif statement edited to match the program condition
