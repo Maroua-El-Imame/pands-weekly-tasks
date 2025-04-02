dd5a972a567c3401ada9c5c8f61caee35c7756b1
Maroua El Imame <g00472980@atu.ie>
2025-04-02 02:58:50 +0100

    Weekly task 06 - incomplete

---
+++ squareRoot.py
@@ -0,0 +1,23 @@
+# squareRoot.py
+# creating 'sqrt' function.
+# This program that takes a positive floating-point number as input and outputs an approximation of its square root.
+# This program is not using python built-in functions x ** .5 or math.sqrt(x)
+# Author : Maroua EL imame
+
+# steps :
+#   1. understand the question:
+#   What's a squareRoot ?
+#   The square root of a number is a number that when multiplied by itself gives the actual number.
+
+#   2. Identify the arguments needed for the calculation.
+#    we need the result(number) to which we try and find its square root(a number when multiplied by itself prooducts the result(number))
+
+#   3. what's the desired outcome ?
+#   The square root of a result(number)
+
+#   Example : The square root of a number is a value that, when multiplied by itself, gives the original number. In other words:
+#               We know that 5 × 5 = 25, in this case, the square root of 25 is 5.
+#   Let's find out how to identify the square root.
+
+# ---------------------------
+# Newton method :
