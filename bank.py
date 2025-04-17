e681e61db502d0f449407b47b91c97bf7fba4a9a
Maroua El Imame <g00472980@atu.ie>
2025-04-17 14:51:45 +0100

    Weekly Task 012 : Clean up and code review

--- bank.py
+++ bank.py
@@ -2,9 +2,16 @@
 # this program prints in euros the sum of two amounts in cents
 # Author: Maroua EL imame
 
-amount1 = 65
-amount2 = 180
+# Asking user to enter a first amount then 2nd amount in cents
+# int so the amount is read as intger not as string.
+amount1  = int(input(" Please enter the first  amount in cents : "))
+amount2  = int(input(" Please enter the second amount in cents : "))   
+
+# sum of two amounts
 amountinCents=amount1+amount2
+
+# sum of amounts in cents when devided by 100 return the amount in euros.
 amountinEuros = (amountinCents)/100
 
-print(f" The sum of ¢{(amount1)} and ¢{(amount2)} is € {amountinEuros}")
+# result
+print(f" The total of amounts you entered is € {amountinEuros}")
